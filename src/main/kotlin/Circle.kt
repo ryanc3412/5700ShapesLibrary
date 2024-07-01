58842c4481caefb74a4cfa23d3de605c8ecfabd0
Ryan Christensen <a023107782@usu.edu>
2024-06-30 21:07:05 -0600

    First implementation of Circle

--- src/main/kotlin/Circle.kt
+++ src/main/kotlin/Circle.kt
@@ -1,4 +1,24 @@
 package org.example
 
-class Circle {
+class Circle(val point: Point, private var radii1: Double, private var radii2: Double) {
+    fun getRadii1(): Double {
+        return radii1
+    }
+    fun getRadii2(): Double {
+        return radii2
+    }
+    fun setRaddi1(radii: Double) {
+        this.radii1 = radii
+    }
+    fun setRadii2(radii: Double) {
+        this.radii2 = radii
+    }
+    fun getArea(): Double {
+        return Math.PI * radii1 * radii2
+    }
+//    moves by a delta along each axis
+    fun moveCircle(x: Double, y: Double) {
+        point.moveX(x)
+        point.moveY(y)
+    }
 }
